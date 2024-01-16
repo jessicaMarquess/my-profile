d3cafe1ddac5fd660f68a65fbe4f1be17e48d751
jessicaMarquess <jessica.marques.dev@gmail.com>
2024-01-16 11:12:33 -0300

    current year in footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import SocialMedias from './components/SocialMedias';
 
 export default function App() {
   const [theme, setTheme] = useState(localStorage.getItem("theme") ? localStorage.getItem("theme") : 'dark');
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+
   const darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
   const element = document.documentElement;
 
@@ -51,7 +53,7 @@ export default function App() {
         <Welcome theme={String(theme)} />
         <SocialMedias theme={String(theme)} />
         <footer className='border border-x-0 border-b-0 border-slate-700 dark:border-slate-300 text-center p-4 fixed bottom-0 w-full text-slate-700 dark:text-slate-300'>
-          FOOTER - MAS AINDA NÃO SEI O QUE COLOCAR AQUI RS
+          Jessica Marques - {currentYear}
         </footer>
       </div>
     </>
