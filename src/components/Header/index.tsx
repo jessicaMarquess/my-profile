1674ba0fad6362e22bec882a0d47695b47546e20
jessicaMarquess <jessica.marques.dev@gmail.com>
2024-01-16 14:15:38 -0300

    change heart emoji

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,7 +10,7 @@ const Header: React.FC<ThemeHeaderProps> = ({ toggleTheme, theme }) => (
   <div className="flex justify-center border border-x-0 border-t-0 border-slate-700 dark:border-slate-300">
     <div className="w-[67.5rem] p-4 pr-0 lg:pr-4 flex justify-between mb-1">
       <h1 className='dark:text-slate-300 text-2xl sm:text-3xl font-semibold font-roboto-mono text-slate-700 lg:-ml-4'>
-        Bem-vindos!🖤
+        Bem-vindos!💜
       </h1>
       <ThemeToggle toggleTheme={toggleTheme} theme={theme || ''} />
     </div>
