5f5ce3600267b146c3f723ca5f51c44291f826ce
jessicaMarquess <jessica.marques.dev@gmail.com>
2024-01-18 20:17:51 -0300

    ajuste

--- src/pages/Pessoal/index.tsx
+++ src/pages/Pessoal/index.tsx
@@ -1,7 +1,10 @@
 export const Pessoal = () => {
   return (
-    <section className="bg-slate-500 rounded-lg m-16 mx-auto flex justify-center items-center h-24 max-w-[1090px]">
-      <h1 className="text-slate-300">Em construção</h1>
+    <section className="text-slate-100  bg-slate-500 rounded-lg m-16 p-16 mx-auto flex justify-center items-center h-24 max-w-[1090px]">
+      <article>
+        <h1 className="font-roboto-mono text-3xl">Olá!</h1>
+        <p>Não sei o que falar sobre mim... </p>
+      </article>
     </section>
   )
 }
