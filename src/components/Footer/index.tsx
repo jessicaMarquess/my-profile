dea10baae76e0bbf7d35a311591c71b115312d73
jessicaMarquess <jessica.marques.dev@gmail.com>
2024-01-18 20:09:01 -0300

    link in my name

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,14 @@ const Footer = () => {
   const currentYear = new Date().getFullYear()
   return (
     <footer className="hidden sm:inline border border-x-0 border-b-0 border-slate-700 dark:border-slate-300 text-center p-4 fixed bottom-0 w-full text-slate-700 dark:text-slate-300 font-roboto-mono">
-      Jessica Marques - {currentYear}
+      <a
+        href="https://github.com/jessicaMarquess"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Jessica Marques
+      </a>{' '}
+      - {currentYear}
     </footer>
   )
 }
