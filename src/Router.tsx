91d8e3ea4711285571afd2f1f32e535bd6aa2818
jessicaMarquess <jessica.marques.dev@gmail.com>
2024-01-22 19:55:56 -0300

    page not-found

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import { Home } from './pages/Home'
 import { Profissional } from './pages/Profissional'
 import { Pessoal } from './pages/Pessoal'
 import React from 'react'
+import NotFound from './pages/NotFound'
 
 interface ThemeProps {
   theme: string
@@ -15,6 +16,7 @@ export const Router: React.FC<ThemeProps> = ({ theme }) => {
         <Route path="/" element={<Home theme={theme} />} />
         <Route path="/perfil-profissional" element={<Profissional />} />
         <Route path="/perfil-pessoal" element={<Pessoal />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   )
---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,27 @@
+import { ArrowUpRightIcon } from '@heroicons/react/20/solid'
+
+const NotFound = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="w-[67.5rem] p-4 pr-0 lg:pr-4 flex flex-col justify-center mb-1">
+        <h1 className="text-slate-700 dark:text-slate-300 text-4xl font-roboto-mono pb-3">
+          Página não encontrada 😐
+        </h1>
+        <a
+          href="/"
+          className="inline-flex items-center text-purple-400 font-roboto-mono cursor-pointer  hover:underline pb-4"
+        >
+          Voltar para a página inicial
+          <ArrowUpRightIcon className=" ml-1" width={16} height={16} />
+        </a>
+
+        <img
+          src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExeWpqcDdoenBsdHN6ODMwendqZ3BzMzR0Z2VqNmZub2VldjFmbHRzciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/V97oPD3JrHDb16goXV/giphy.gif"
+          alt="Menina chorando"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
